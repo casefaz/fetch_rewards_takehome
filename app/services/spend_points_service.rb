563ef7270139f380ee1422773df6918ad66e1735
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-10-20 18:19:14 -0600

    Add test for positive transactions

--- app/services/spend_points_service.rb
+++ app/services/spend_points_service.rb
@@ -1,6 +1,39 @@
 class SpendPointsService
   # adding a colon after points makes it required (keyword argument with no default)
   def self.call(points:)
-    
+    # grab all the transactions that aren't negative (ordered by timestamp)
+    # reduce (iterate) over them with a hash with default of zero payer_differences
+    # for each element
+      # guard against negative payer balance
+      # next unless the total -  for payer is positive
+      # points - balance down to: points >= 0
+      # update line item with new value - which should be 0
+      # add to payer_differences: the difference
+      # break if points are 0
+    transactions
+    .each_with_object(payer_differences)
+    .reduce(points) do |pnts, (trn, differences)|
+      next if negative_payer_balance(trn)
+      difference = if pnts >= trn.points 
+        trn.points
+      else
+        pnts
+      end
+      break if pnts == 0
+    end
+  end
+
+  def transactions
+    # finds all positive transactions
+    Transaction.where('points > 0')
+  end
+
+  def payer_differences
+    # default proc that says we aren't using v
+    Hash.new { |h,k,_v| h[k] = 0 }
+  end
+
+  def negative_payer_balance(transaction)
+    false
   end
 end
--- spec/services/spend_points_service_spec.rb
+++ spec/services/spend_points_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SpendPointsService do
-  it 'spends points and returns payer with point balance being the difference' do
+  xit 'spends points and returns payer with point balance being the difference' do
     user = User.create!(name: 'Bundles Gerald')
     transaction2 = Transaction.create!(payer: "UNILEVER", points: 200, timestamp: "2020-10-31T11:00:00Z", user_id: user.id) 
     transaction3 = Transaction.create!(payer: "DANNON", points: 200, timestamp: "2020-10-31T15:00:00Z", user_id: user.id)
@@ -10,6 +10,17 @@ RSpec.describe SpendPointsService do
       {"payer": "Dannon", "points": -200},
       {"payer": "UNILEVER", "points": -100}
     ]
-    expect(described_class.(points:300).to eq(expected)
+    expect(described_class.(points:300)).to eq(expected)
+  end
+
+  describe 'helper methods' do
+    it 'finds all of the positive transactions' do
+      spender = SpendPointsService.new
+      user = User.create!(name: 'Maude Green')
+      transaction2 = Transaction.create!(payer: "UNILEVER", points: 200, timestamp: "2020-10-31T11:00:00Z", user_id: user.id) 
+      transaction3 = Transaction.create!(payer: "DANNON", points: -200, timestamp: "2020-10-31T15:00:00Z", user_id: user.id)
+
+      expect(spender.transactions).to eq([transaction2])
+    end
   end
 end
